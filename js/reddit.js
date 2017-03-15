39850c0bde8e239cf40c3709bba39d04c7c719b3
Justin Hart <justinhart90@gmail.com>
2017-03-15 12:58:12 -0600

    updated headers with logos

--- js/reddit.js
+++ js/reddit.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  getNews()
+  getRedditNews()
 });
 
-function getNews(){
+function getRedditNews(){
   var url = "https://www.reddit.com/r/OutOfTheLoop/.json";
 
   $.get(url)
@@ -11,6 +11,7 @@ function getNews(){
     resultsCounter = 0;
     resultsTitle = [];
     resultsUrl = [];
+    resultsDescr = [];
     duplicateIndicator = false;
 
     while (resultsCounter < 6) {
@@ -18,10 +19,13 @@ function getNews(){
         if (newsArray[i].data.link_flair_text === "Answered" && !newsArray[i].data.title.includes("r/") && !newsArray[i].data.title.includes("Why are people randomly spoiling the ending of Logan in the comments?")) {
           resultsTitle.push(newsArray[i].data.title);
           resultsUrl.push(newsArray[i].data.url);
-          resultsCounter++
+          resultsDescr.push(newsArray[i].data.selftext);
+          resultsCounter++;
         }
       }
     }
+    console.log(resultsDescr);
+
     $('#title1').text(resultsTitle[0]);
     $('#title2').text(resultsTitle[1]);
     $('#title3').text(resultsTitle[2]);
@@ -33,21 +37,13 @@ function getNews(){
     $('#link3').attr("href", resultsUrl[2]);
     $('#link4').attr("href", resultsUrl[3]);
     $('#link5').attr("href", resultsUrl[4]);
+
+    $('#link1').attr("title", resultsDescr[0]);
+    $('#link2').attr("title", resultsDescr[1]);
+    $('#link3').attr("title", resultsDescr[2]);
+    $('#link4').attr("title", resultsDescr[3]);
+    $('#link5').attr("title", resultsDescr[4]);
   })
-    // var title_1 = data.data.children[1].data.title
-    // var url_1 = data.data.children[1].data.url
-    //
-    // var title_2 = data.data.children[2].data.title
-    // var url_2 = data.data.children[2].data.url
-    //
-    // var title_3 = data.data.children[3].data.title
-    // var url_3 = data.data.children[3].data.url
-    //
-    // var title_4 = data.data.children[4].data.title
-    // var url_4 = data.data.children[4].data.url
-    //
-    // var title_5 = data.data.children[5].data.title
-    // var url_5 = data.data.children[5].data.url
   .catch(function(error) {
     console.log(error);
   })
