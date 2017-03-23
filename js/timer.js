c8bdddcfa259a83f4a2ae594df1df055d1e2d465
Justin Hart <hart@Justins-MacBook.local>
2017-03-23 01:21:00 -0600

    minor styling adjustment to nav bar

---
+++ js/timer.js
@@ -0,0 +1,22 @@
+$(document).ready( function() {
+  $("timer").click( function() {
+    getTimeRemaining(deadline);
+  })
+})
+
+var deadline = $("timer").val();
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
